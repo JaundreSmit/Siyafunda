589cad95c9fce321dd246b129c1adcee60c69f7d
JaundreSmit <jaundre123.smit@gmail.com>
2024-09-20 16:14:17 +0200

    Created ASP.NET MVC Files
    Most back end basic structure and potential for front end is now in place

---
+++ SiyafundaApplication/Models/LeadEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace SiyafundaApplication.Models
+{
+    public class LeadEntity
+    {
+        public int TestID { get; set; }
+        public int Name { get; set; }
+        public int Surname { get; set; }
+    }
+}
