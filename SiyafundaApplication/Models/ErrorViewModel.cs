a92affd9f037afc5dc388b680fc394a297d2675b
JaundreSmit <jaundre123.smit@gmail.com>
2024-09-24 15:35:56 +0200

    Reworked the framework
    Switched from MVC controller to .Net framework

--- SiyafundaApplication/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SiyafundaApplication.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
