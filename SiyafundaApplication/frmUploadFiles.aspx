82725c3c5085ded36e01c3e95987c1e658a189da
Renier van Rooyen <reniervanrooyen767@gmail.com>
2024-09-26 13:19:16 +0200

    New file upload

---
+++ SiyafundaApplication/frmUploadFiles.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="frmUploadFiles.aspx.cs" Inherits="SiyafundaApplication.frmUploadFiles" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Button ID="UploadButton" runat="server" OnClick="UploadButton_Click" Text="Button" />
+        <asp:FileUpload ID="FileUploadControl" runat="server" />
+    </form>
+</body>
+</html>
---
+++ SiyafundaApplication/frmUploadFiles.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SiyafundaApplication
+{
+    public partial class frmUploadFiles : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+            if (FileUploadControl.HasFile)
+            {
+                //Path to the UploadedFiles directory
+                string uploadDir = Server.MapPath("~/UploadedFiles/");
+
+                //Makes sure the directory exists
+                if (!Directory.Exists(uploadDir))
+                {
+                    Directory.CreateDirectory(uploadDir);
+                }
+
+                // Combines the directory path with the uploaded file name
+                string filePath = Path.Combine(uploadDir, FileUploadControl.FileName);
+
+                try
+                {
+                    //TODO: sync file upload with DB, make sure path is correct to each module and implement moderation of files
+                    //Save the uploaded file to the specified path
+                    FileUploadControl.SaveAs(filePath);
+
+                    //Success
+                    Response.Write("File uploaded successfully! Path: " + filePath);
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("Error occurred: " + ex.Message);
+                }
+            }
+            else
+            {
+                // No file selected
+                Response.Write("No file selected.");
+            }
+        }
+    }
+}
