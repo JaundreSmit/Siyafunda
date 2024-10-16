bfd458aa47b383a04042b783ef0c5c8f1b4dd915
Renier van Rooyen <reniervanrooyen767@gmail.com>
2024-10-16 19:56:40 +0200

    Updated AddAnouncement

--- SiyafundaApplication/frmAddAnnouncement.aspx.designer.cs
+++ SiyafundaApplication/frmAddAnnouncement.aspx.designer.cs
@@ -51,15 +51,6 @@ namespace SiyafundaApplication
         protected global::System.Web.UI.WebControls.Label lblErrors;
 
         /// <summary>
-        /// Back control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Back;
-
-        /// <summary>
         /// btnAddAnnouncement control.
         /// </summary>
         /// <remarks>
@@ -67,5 +58,14 @@ namespace SiyafundaApplication
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnAddAnnouncement;
+
+        /// <summary>
+        /// Back control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Back;
     }
 }
