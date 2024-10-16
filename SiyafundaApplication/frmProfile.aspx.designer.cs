c994ca83e333d67cc6cbc79978d4b57fe0121fda
Renier van Rooyen <reniervanrooyen767@gmail.com>
2024-10-16 17:34:11 +0200

    Updated Profile

--- SiyafundaApplication/frmProfile.aspx.designer.cs
+++ SiyafundaApplication/frmProfile.aspx.designer.cs
@@ -105,15 +105,6 @@ namespace SiyafundaApplication
         protected global::System.Web.UI.WebControls.Button SaveButton;
 
         /// <summary>
-        /// lblErrors control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblErrors;
-
-        /// <summary>
         /// btnBack control.
         /// </summary>
         /// <remarks>
@@ -121,6 +112,15 @@ namespace SiyafundaApplication
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnBack;
+
+        /// <summary>
+        /// lblErrors control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblErrors;
 
         /// <summary>
         /// ModulesGridView control.
