0b993f175ce25cc2318d8a3fce0a34f5585b84ac
JaundreSmit <jaundre123.smit@gmail.com>
2024-10-23 21:45:28 +0200

    UI Fix + Azure SQL packages

--- SiyafundaApplication/frmModerator.aspx.designer.cs
+++ SiyafundaApplication/frmModerator.aspx.designer.cs
@@ -15,13 +15,13 @@ namespace SiyafundaApplication
     {
 
         /// <summary>
-        /// pnlInProgress control.
+        /// btnBack control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel pnlInProgress;
+        protected global::System.Web.UI.WebControls.Button btnBack;
 
         /// <summary>
         /// pnlInProgress control.
